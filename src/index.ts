e01c8440e5b2606bb610cfb21572fd5f5788a8d1
Kamil Kamiński <kkaminski9311@gmail.com>
2020-08-13 23:27:20 +0200

    Add game loop

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,26 @@
-const canvas: HTMLCanvasElement | null = document.querySelector('canvas');
+import { assert } from './utils';
 
-if (!canvas) {
-  throw new Error('No canvas in the DOM');
-}
+const canvas: HTMLCanvasElement | null = document.querySelector('canvas');
+assert(canvas, 'No canvas');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const ctx = canvas.getContext('2d');
-if (!ctx) {
-  throw new Error("Couldn't get 2d canvas context");
-}
+window.addEventListener('resize', () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
 
-ctx.fillStyle = '#FF0000';
-ctx.fillRect(0, 0, 100, 100);
+const gl = canvas.getContext('webgl2');
+assert(gl, 'No webgl2');
+
+gl.clearColor(0, 0, 0, 1.0);
+
+const gameLoop = () => {
+  gl.viewport(0, 0, canvas.width, canvas.height);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  window.requestAnimationFrame(gameLoop);
+};
+
+window.requestAnimationFrame(gameLoop);
