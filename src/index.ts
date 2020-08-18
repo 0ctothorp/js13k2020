8fd003c9e110f016b67f74393c0312aeb5187df7
Kamil Kamiński <kkaminski9311@gmail.com>
2020-08-18 23:38:01 +0200

    Bring back snowpack and add some more webgl boilerplate from the other project

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { assert } from './utils.js';
+import { assert } from './utils';
 
 const canvas: HTMLCanvasElement | null = document.querySelector('canvas');
 assert(canvas, 'No canvas');
@@ -53,7 +53,103 @@ const makeTriangleStrapIndices = (width: number, height: number) => {
 };
 
 const indices = makeTriangleStrapIndices(w, h);
-console.log({ indices });
+
+function createShader(
+  gl: WebGL2RenderingContext,
+  type: number,
+  source: string,
+) {
+  const shader = gl.createShader(type);
+  assert(shader, "Couldn't create shader");
+
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+
+  const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+  if (success) {
+    return shader;
+  }
+
+  console.log("Couldn't compile the shader: " + gl.getShaderInfoLog(shader));
+  gl.deleteShader(shader);
+}
+
+function createProgram(
+  gl: WebGL2RenderingContext,
+  vertexShader: WebGLShader,
+  fragmentShader: WebGLShader,
+) {
+  const program = gl.createProgram();
+  assert(program, "Couldn't link the shader program.");
+
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+
+  const success = gl.getProgramParameter(program, gl.LINK_STATUS);
+  if (success) {
+    return program;
+  }
+
+  console.log(gl.getProgramInfoLog(program));
+  gl.deleteProgram(program);
+}
+
+const createShaderProgram = (
+  gl: WebGL2RenderingContext,
+  vertexShaderCode: string,
+  fragShaderCode: string,
+) => {
+  const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderCode);
+  assert(vertexShader, "Couldn't create vertex shader");
+
+  const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragShaderCode);
+  assert(fragmentShader, "Couldn't create fragment shader");
+
+  const program = createProgram(gl, vertexShader, fragmentShader);
+  assert(program, "Couldn't create shader program");
+
+  return program;
+};
+
+const planeVertexShader = `#version 300 es
+
+    in vec2 aPosition;
+
+    uniform mat4 uModelMat;
+    uniform mat4 uProjMat;
+    
+    void main() {
+      gl_Position = uProjMat * uModelMat * vec4(aPosition, 0, 1);
+    }
+  `;
+
+const planeFragmentShader = `#version 300 es
+  
+    // fragment shaders don't have a default precision so we need
+    // to pick one. highp is a good default. It means "high precision"
+    precision highp float;
+
+    out vec4 outColor;
+
+    uniform vec4 uColor;
+
+    void main() {
+      outColor = uColor;
+    }
+  `;
+
+const program = createShaderProgram(gl, planeVertexShader, planeFragmentShader);
+
+const posAttrLoc = gl.getAttribLocation(program, 'aPosition');
+
+const vao = gl.createVertexArray();
+gl.bindVertexArray(vao);
+
+const posBuffer = gl.createBuffer();
+assert(posBuffer, "Couldn't create position buffer");
+gl.bindBuffer(gl.ARRAY_BUFFER, posBuffer);
+gl.bufferData(gl.ARRAY_BUFFER, Float32Array.from(vertices), gl.STATIC_DRAW);
 
 gl.clearColor(0, 0, 0, 1);
 
